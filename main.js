74b01de2214961636808708d9eec22543cd3bc17
Jedidiah Miller <106718706+JedidiahMiller@users.noreply.github.com>
2023-01-13 13:47:41 -0500

    slider class created

--- main.js
+++ main.js
@@ -4,45 +4,61 @@ var baseColor = {"r": 0, "g":0, "b":0};
 
 window.onload = () => {
 
-    const colorSquare = document.getElementById("colorSquare");
 
-    // Details square
-    colorSquare.addEventListener("mousedown", (e)=>{colorSquareIsDragging=true;position(e);});
-    window.addEventListener("mouseup", ()=>{colorSquareIsDragging=false;});
-    window.addEventListener("mousemove", (e) => {position(e)});
+
+    const box = document.getElementById("colorSquare");
+    const slider = document.getElementById("colorSelector");
+    const mainSlider = new Slider(box, slider);
+    
 
 }
 
-function position(event) {
+class Slider {
 
-    if (colorSquareIsDragging) {
+    constructor (box, slider) {
+        this.box = box;
+        this.slider = slider;
+        this.isSliding = false;
+        
+        this.box.addEventListener("mousedown", (event)=>{this.isSliding=true;this.drag(event);});
+        window.addEventListener("mouseup", ()=>{this.isSliding=false;});
+        window.addEventListener("mousemove", (event) => {this.drag(event);});
 
-        const box = document.getElementById("colorSquare").getBoundingClientRect();
+        console.log(box.x);
 
-        var x = 100 * (event.clientX - box.x) / box.width;
-        var y = 100 * (event.clientY - box.y) / box.height;
+    }
 
-        if (x > 100) {
-            x = 100;
-        } 
-        if (x < 0) {
-            x = 0;
-        } 
-        if (y > 100) {
-            y = 100;
-        } 
-        if (y < 0) {
-            y = 0;
-        } 
+    drag(event) {
 
-        const picker = document.getElementById("colorSelector");
+        if (this.isSliding) {
 
-        picker.style.top = `${y-4}%`;
-        picker.style.left = `${x-4}%`;
-        picker.style.background = `hsl(${100}, ${x}%, ${y}%)`;
+            const boundingBox = this.box.getBoundingClientRect();
+
+            var x = (event.clientX - boundingBox.left) / this.box.clientWidth;
+            var y = (event.clientY - boundingBox.top) / this.box.clientHeight;
+
+            if (x > 1) {
+                x = 1;
+            } 
+            if (x < 0) {
+                x = 0;
+            } 
+            if (y > 1) {
+                y = 1;
+            } 
+            if (y < 0) {
+                y = 0;
+            } 
+    
+            this.slider.style.left = `${(x * this.box.clientWidth) - (this.slider.clientWidth / 2)}px`;
+            this.slider.style.top = `${(y * this.box.clientHeight) - (this.slider.clientHeight / 2)}px`;
+
+        }
     }
 
 }
 
 
 // 255 0 0, 255 255 0, 0 255 0, 
+// https://cssgradient.io/
+// https://rgbacolorpicker.com/
